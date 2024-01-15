f589605085c073b1161506b8f5512b073892f446
KIM Hyun Woo <155598824+kimhyunuoo@users.noreply.github.com>
2024-01-15 18:56:09 +0900

    [level Lv.0] Title: 각도기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120829. 각도기/각도기.js
@@ -0,0 +1,13 @@
+function solution(angle) {
+    var answer = 0;
+    if (angle < 90) {
+        return 1;
+    } else if (angle == 90) {
+        return 2;
+    } else if (angle > 90 && angle < 180) {
+        return 3;
+    } else (angle == 180) 
+        return 4;
+    
+    return angle;
+}
