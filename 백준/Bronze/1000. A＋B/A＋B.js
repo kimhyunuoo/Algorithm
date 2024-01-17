d86742997625dab4392467a0d3c9b5a0cb25a0a0
KIM Hyun Woo <155598824+kimhyunuoo@users.noreply.github.com>
2024-01-17 16:53:13 +0900

    [Bronze V] Title: A+B, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,7 +1,9 @@
-const fs = require('fs');
-const inputData = fs.readFileSync(0, 'utf8').toString().split(' ');
+let fs = require("fs");
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-const A = parseInt(inputData[0]);
-const B = parseInt(inputData[1]);
+let line =input[0].split(' ');
 
-console.log(A+B);
+let a = parseInt(line[0]);
+let b = parseInt(line[1]);
+
+console.log(a + b);
