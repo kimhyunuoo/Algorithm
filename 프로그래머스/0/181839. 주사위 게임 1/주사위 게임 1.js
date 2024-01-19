9b58942bf6d9ee172694beafc6593e856f376c4a
KIM Hyun Woo <155598824+kimhyunuoo@users.noreply.github.com>
2024-01-19 17:06:14 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+    if (a % 2 === 1 && b % 2 === 1) {
+        return a * a + b * b;
+    } else if (a % 2 === 1 || b % 2 === 1) {
+        return 2 * (a + b);
+    } else {
+        return Math.abs(a - b);
+    }
+}
+let result = solution(3, 5);
+console.log(result);
